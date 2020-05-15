60a04d2d1fcb61d4343ba868ace218d8d4e3bf84
Tomoki Mizogami <tomoki.mizogami@nextbeat.net>
2020-05-15 11:09:51 +0900

    Move main.scss to stylesheets/

---
+++ memos/May15th.md
@@ -0,0 +1,18 @@
+# 2020-05-15/Fri
+
+## プロジェクトのディレクトリ構成
+
+- index.html
+- main.css
+- package.json
+- webpack.config.js
+- stylesheets/
+  - main.scss
+  - header.scss
+  - footer.scss
+  - sidebar.scss
+  - button.scss
+
+## 設計方針
+
+BEM を用いて CSS 設計をする。
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, './'),
+  // ここがどのscssをみるか
+  entry: "./stylesheets/main.scss",
+  output: {    
+    // どのディレクトリにどんな名前でbuild後のcssを吐くか
+    path: path.join(__dirname, ''),
+    filename: '[name].css'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        // 使用するプラグイン
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [    new ExtractTextPlugin('[name].css')  ]
+}
